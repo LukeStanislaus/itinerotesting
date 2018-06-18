677972f1cac436a8a4a8c013a8444dff560d9975
Luke Stanislaus <luke@stanislaus.co.uk>
2018-06-18 21:09:40 +0100

    Added conrec, testing ToGeoJSON

--- itinerotesting/Program.cs
+++ itinerotesting/Program.cs
@@ -1,17 +1,38 @@
+using Itinero;
+using Itinero.IO.Osm;
+using Itinero.LocalGeo;
+using Itinero.Osm.Vehicles;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace itinerotesting
 {
     class Program
     {
+        public static List<Coordinate> coordlist = new List<Coordinate>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+
+            // load some routing data and build a routing network.
+            var routerDb = new RouterDb();
+
+            using (var stream = new FileInfo(@"C:\Users\Luke\Downloads\downloads\saint-helena-ascension-and-tristan-da-cunha-latest.osm.pbf").OpenRead())
+            {
+                // create the network for cars only.
+                routerDb.LoadOsmData(stream, Vehicle.Car);
+            }
+            routerDb.area
+            Router router = new Router(routerDb);
+            // get a profile.
+            var profile = Vehicle.Car.Fastest(); // the default OSM car profile.
+
+
+            var start = router.Resolve(profile, -15.9244f, -5.718f);
+            var end = router.Resolve(profile, -15.9245f, -5.719f);
+            var route = router.Calculate(profile, start, end);
+            Console.WriteLine(route.ToGeoJson());
+            Console.ReadLine();
         }
     }
 }
